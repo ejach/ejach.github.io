fa39f1e12d9a068645069c0d9c51cab7750c7205
poopi67 <42357644+ejach@users.noreply.github.com>
2021-04-13 11:41:16 -0400

    Added contact form

---
+++ showForm.js
@@ -0,0 +1,8 @@
+function showForm() {
+  x =  document.getElementById('formElement');
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
