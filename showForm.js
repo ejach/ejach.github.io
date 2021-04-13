d31c5c2785bc0dd5ebb96f618333e0db38039eeb
poopi67 <42357644+ejach@users.noreply.github.com>
2021-04-13 13:13:39 -0400

    test

--- showForm.js
+++ showForm.js
@@ -1,7 +1,3 @@
-document.addEventListener('touchmove', function (event) {
-  if (event.scale !== 1) { event.preventDefault(); }
-}, false);
-
 function showForm() {
   x =  document.getElementById('formElement');
   if (x.style.display === "none") {
