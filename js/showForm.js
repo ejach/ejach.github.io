cf3ebb518a90fd3490037ff90912f3539c75f699
Evan J <42357644+ejach@users.noreply.github.com>
2021-05-20 15:07:08 -0400

    Used DOM selector
    Used DOM selector instead of calling the function from the HTML directly.

--- js/showForm.js
+++ js/showForm.js
@@ -1,9 +1,11 @@
 // Function that toggles/hides the contact form
-function showForm() {
-  x =  document.getElementById('formElement');
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
+window.onload = function() {
+  document.getElementById("showForm").addEventListener("click", function() {
+      x = document.getElementById('formElement');
+      if (x.style.display === "none") {
+          x.style.display = "block";
+      } else {
+          x.style.display = "none";
+      }
+  });
 }
