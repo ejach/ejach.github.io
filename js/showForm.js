73f6f870c69d8623eafb8e8550b28deb32b0f556
ejach <42357644+ejach@users.noreply.github.com>
2021-06-16 12:19:09 -0400

    Refactoring, a new hope

--- js/showForm.js
+++ js/showForm.js
@@ -1,9 +1,6 @@
+// Function to show/display the contact form
 $(document).ready(function() {
-  $('input').css('display', 'none');
-  $('textarea').css('display', 'none');
     $("#showForm").click(function() {
-      $("#formElement").fadeToggle(400);
-      $('input').fadeToggle(600);
-      $('textarea').fadeToggle(600);
+      $("#formElement").slideToggle();
     });
   });
