241f5ae22bc00533fe86d3e6a99ef57140d65feb
ejach <42357644+ejach@users.noreply.github.com>
2021-06-16 11:41:00 -0400

    Refactoring, chapter 5

--- js/showForm.js
+++ js/showForm.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $('input').css('display', 'none');
   $('textarea').css('display', 'none');
     $("#showForm").click(function() {
-      $("#formElement").fadeToggle(600);
+      $("#formElement").fadeToggle(400);
       $('input').fadeToggle(600);
       $('textarea').fadeToggle(600);
     });
